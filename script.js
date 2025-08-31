ff15714f95413a882a425f327ee9f3e3970ae6ce
naimsheikh8020 <naimsheikh8020@gmail.com>
2025-08-31 06:34:21 +0600

    gsap

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 gsap.to("#box",{
     x:1000,
     duration:2,
-    delay:1
+    delay:1,
+    borderRadius: "50%",
+    scale: 0.5,
+    backgroundColor: "white",
+    repeat:3,
+    yoyo:true
+})
+gsap.from("h1",{
+    opacity:0,
+    duration:2,
+    y:1000,
+    delay:1,
+    stagger:0.3
 })
